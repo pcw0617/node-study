7e2425a15353c69ade1cc3b155885dd7f52717f7
pcw0617 <08831@skcc.com>
2016-10-13 16:39:25 +0900

    add package.json

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 console.log('야호!');
+
+var http = require('http');
+
+http.createServer(function(request, response) {
+  var headers = request.headers;
+  var method = request.method;
+  var url = request.url;
+  var body = [];
+
+  console.log(`-> ${request.method} ${request.url}`);
+
+  response.end('hello!');
+
+}).listen(8080); // 이 서버를 활성화하고 8080 포트로 받습니다.
